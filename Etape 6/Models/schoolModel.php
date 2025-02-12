58215c93d29052c7648690c826a4e136218b1745
Nathan030206 <230064@site.asty-moulin.be>
2025-02-12 19:09:02 +0100

    Fini et facile

--- Etape 6/Models/schoolModel.php
+++ Etape 6/Models/schoolModel.php
@@ -51,4 +51,72 @@ function DeleteUser($pdo)
 
 }
 
+function selectMySchools($pdo)
+{
+    try {
+        $query = 'select * from school where utilisateurId = :utilisateurId';
+        $selectSchool = $pdo->prepare($query);
+        $selectSchool->executte([
+            'utilisateurId' =>  $_SESSION["user"]->id
+        ]);
+        $schools = $selectSchool->fetchAll();
+        return $schools;
+    } catch (PDOException $e) {
+        $message = $e->getMessage();
+        die($message);
+    }
+}
+
+function selectAllOptions($pdo)
+{
+    try{
+        $query ='SELECT * FROM optionscolaire';
+        $selectOptions = $pdo->prepare($query);
+        $selectOptions->fetchAll();
+        $options = $selectOptions->fetchAll();
+        return $options;
+    } catch (PDOException $e)
+    {
+        $message = $e->getMessage();
+        die($message);
+    }
+}
+
+function createSchool($pdo)
+{
+    try 
+    {
+        $query = 'insert into school (schoolNom, schoolAdresse, schoolVille, schoolCodePostal, schoolNumero, schoolImage, utilisateurId)
+        values (:schoolNom, :schoolAdresse, :schoolVille, :schoolCodePostal, :schoolNumero, :schoolImage, :utilisateurId)';
+        $addSchool = $pdo->prepare($query);
+        $addSchool->execute([
+            'schoolNom' => $_POST["nom"],
+            'schoolAdresse' =>$_POST["adresse"],
+            'schoolVille' =>$_POST["ville"],
+            'schoolCodePostal' =>$_POST["code_postal"],
+            'schoolNumero' =>$_POST["numero_telephone"],
+            'schoolImage' =>$_POST["image"],
+            'utilisateurId' => $_SESSION["user"]->id
+        ]);
+    } catch (PDOException $e) {
+        $message = $e->getMessage();
+        die($message);
+    }
+}
+
+function ajouterOptionEcole($pdo,$schoolId,$optionId)
+{
+    try {
+        $query='insert into option_ecole (schoolId, optionScolaireId) values (:schoolId, :optionScolaireId)';
+        $deleteAllSchoolsFromId = $pdo->prepare($query);
+        $deleteAllSchoolsFromId->execute ([
+            'schoolId' => $schoolId,
+            'optionScolaireId' => $optionId
+        ]);
+    } catch (\PDOException $e) {
+        $message = $e->getMessage();
+        die($message);
+    }
+}
+
 ?>
--- Etape 6/Views/pageAccueil.php
+++ Etape 6/Views/pageAccueil.php
@@ -1,4 +1,11 @@
-
+<?php if ($uri === "/mesEcoles") : ?>
+    <h1>Vos écoles</h1>
+<?php else : ?>
+    <h1>Liste des écoles répertoriées</h1>
+<?php endif ?>
+<?php if (isset($_SESSION["user"])) : ?>
+    <a href="createSchool">Ajouter une école</a>
+<?php endif ?>
 <h1>Liste des écoles répertoriées</h1>
 
 <div class="flexible wrap space-around">
@@ -13,6 +20,10 @@
                 <p><span><?=$school->schoolAdresse ?> - </span> <span><?= $school->schoolCodePostal . " " . $school->schoolVille ?></span></p>
                 <h3><?=$school->schoolNumero ?></h3>
                 <a href="voirEcole.php" class="btn btn-page">Voir l'école</a>
+                <?php if ($uri === "/mesEcoles") : ?>
+                    <p><a href="deleteEcole?schoolId=<?= $school->schoolId ?>">Supprimer l'école</a></p>
+                    <p><a href="updateEcole?schoolId=<?= $school->schoolId ?>"></a>Modifier l'école</p>
+                <?php endif ?>
             </div>
         </div>
     </div>
